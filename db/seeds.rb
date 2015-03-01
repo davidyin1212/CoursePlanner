6ed1025e7b168e57f296cbb4f1f6a0edf78766e1
Sairon Istyari <Joshuak.lee@mail.utoronto.ca>
2015-03-01 12:30:15 -0500

    Database seeding works

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,12 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-course_json_file = File.read('master_timetable_partial.json')
+course_json_file = File.read('db/master_timetable_partial.json')
 #course_json_file = File.read('all_courses.json')
-
+#Course.create({course_name: 'abc'})
 data_hash = JSON.parse(course_json_file)
 data_hash.default = ''
 
 data_hash.each do |course_code, sub_hash|
-	Course.create(course_code: sub_hash['course_code'], course_name: sub_hash['course_name'], course_description: sub_hash['course_description'],
-		created_at: 0
-		updated_at: 0
-		Lec: sub_hash['LEC'], Pra: sub_hash['PRA'], Fallsections: sub_hash['fallsections'], Tut: sub_hash['TUT'], Code: course_code
-		Wintersections: sub_hash['wintersections'], Sc: sub_hash['SC'], Cr: sub_hash['CR'], Sem: sub_hash['SEM'])
+	res = Course.create({course_code: sub_hash['course_code'], course_name: sub_hash['course_name'], course_description: sub_hash['course_description'], created_at: 0, updated_at: 0, Lec: sub_hash['LEC'], Pra: sub_hash['PRA'], Fallsections: sub_hash['fallsections'], Tut: sub_hash['TUT'], Code: course_code, Wintersections: sub_hash['wintersections'], Sc: sub_hash['SC'], Cr: sub_hash['CR'], Sem: sub_hash['SEM']})
 end
